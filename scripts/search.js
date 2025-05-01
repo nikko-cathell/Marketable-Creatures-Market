f3a6c12c591560759e86788bc47b5461bbf9db52
nikko-cathell <mc33266@email.vccs.edu>
2025-05-01 18:09:38 -0400

    The shop page had hands but I had more.. I grew 4 more hands just to finish it. I have that much determination. I beat science. Anyways I also added the desktop about us page. The hamburger menu still evades me but maybe with my extra 4 hands I can catch it soon.

---
+++ scripts/search.js
@@ -0,0 +1,19 @@
+// Search
+function search() {
+  var input, filter, ul, li, div, h6, txtValue;
+  input = document.getElementById('find');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("shopables");
+  li = ul.getElementsByTagName('div');
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("h6")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
